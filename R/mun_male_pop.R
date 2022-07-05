605cde1ff15dac0438d7db22e07c355ea28964d5
Raphael Saldanha <rfsaldanha@gmail.com>
2022-07-05 09:38:45 -0300

    Dataset documentation.

---
+++ R/mun_male_pop.R
@@ -0,0 +1,17 @@
+#' Municipality yearly male population estimates per age group
+#'
+#' A dataset containing male population estimates for Brazilian municipalities per age groups from 2000 to 2021.
+#'
+#' The estimates were calculated by DataSUS (Brazilian Ministry of Health), manually downloaded from DataSUS website, and organized as a tibble.
+#'
+#' More details about the estimation methodology may be found here: \url{http://tabnet.datasus.gov.br/cgi/POPSVS/NT-POPULACAO-RESIDENTE-2000-2021.PDF}
+#'
+#' @format A tibble with 2,216,016 rows and 4 variables:
+#' \describe{
+#'   \item{mun}{municipality 6 digits code}
+#'   \item{year}{year of the estimative}
+#'   \item{age_group}{age group}
+#'   \item{pop}{population estimative}
+#' }
+#' @source \url{http://tabnet.datasus.gov.br/cgi/deftohtm.exe?popsvs/cnv/popbr.def}
+"mun_male_pop"
