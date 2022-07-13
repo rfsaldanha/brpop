03e44d502e5aade6e8416f88629d7f6ac210e0c0
Raphael Saldanha <rfsaldanha@gmail.com>
2022-07-13 13:04:30 -0300

    Mun pop totals.

---
+++ R/uf_pop_totals.R
@@ -0,0 +1,29 @@
+#' UF yearly population estimates totals
+#'
+#' This function provides a tibble containing population estimates for Brazilian UFs ("Unidades Federativas") totals from 2000 to 2021.
+#'
+#' The estimates were calculated by DataSUS (Brazilian Ministry of Health), manually downloaded from DataSUS website, and organized as a tibble.
+#'
+#' More details about the estimation methodology may be found here: \url{http://tabnet.datasus.gov.br/cgi/POPSVS/NT-POPULACAO-RESIDENTE-2000-2021.PDF}
+#'
+#' \describe{
+#'   \item{uf}{UF 2 digits code}
+#'   \item{year}{year of the estimative}
+#'   \item{pop}{population estimative}
+#' }
+#'
+#' @returns A tibble.
+#' @seealso [uf_male_pop], [uf_female_pop].
+#'
+#' @importFrom rlang .data
+#' @export
+
+uf_pop_totals <- function(){
+  res <- dplyr::bind_rows(brpop::uf_male_pop, brpop::uf_female_pop) %>%
+    dplyr::group_by(.data$uf, .data$year) %>%
+    dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(.data$uf, .data$year, .data$pop)
+
+  return(res)
+}
