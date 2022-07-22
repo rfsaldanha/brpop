93e767b9f75d49f0f6265625e220570b7029f93e
Raphael Saldanha <rfsaldanha@gmail.com>
2022-07-22 17:24:29 -0300

    Adds municipality and health region code table.

---
+++ R/mun_reg_saude.R
@@ -0,0 +1,10 @@
+#' Municipality and health region code table
+#'
+#' A dataset containing municipality and health regions codes.
+#'#'
+#' @format A tibble with 5,597 rows and 2 variables:
+#' \describe{
+#'   \item{cod_mun}{municipality 6 digits code}
+#'   \item{cod_reg_saude}{health region 4 digits code}
+#' }
+"mun_reg_saude"
