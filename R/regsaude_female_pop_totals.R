c5fee70a931ab9f538c61bad76af3e10c802be7e
Raphael Saldanha <rfsaldanha@gmail.com>
2022-07-27 19:42:04 -0300

    Creates functions for male and female population totals.

---
+++ R/regsaude_female_pop_totals.R
@@ -0,0 +1,29 @@
+#' Health region yearly female population estimates totals
+#'
+#' This function provides a tibble containing female population estimates for Brazilian health regions totals from 2000 to 2021.
+#'
+#' The estimates were calculated by DataSUS (Brazilian Ministry of Health), manually downloaded from DataSUS website, and organized as a tibble.
+#'
+#' More details about the estimation methodology may be found here: \url{http://tabnet.datasus.gov.br/cgi/POPSVS/NT-POPULACAO-RESIDENTE-2000-2021.PDF}
+#'
+#' \describe{
+#'   \item{regsaude}{health region 4 digits code}
+#'   \item{year}{year of the estimative}
+#'   \item{pop}{population estimative}
+#' }
+#'
+#' @returns A tibble.
+#' @seealso [regsaude_female_pop].
+#'
+#' @importFrom rlang .data
+#' @export
+
+regsaude_female_pop_totals <- function(){
+  res <- regsaude_female_pop() %>%
+    dplyr::group_by(.data$regsaude, .data$year) %>%
+    dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(.data$regsaude, .data$year, .data$pop)
+
+  return(res)
+}
