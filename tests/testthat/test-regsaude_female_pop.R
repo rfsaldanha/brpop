28fd76d808bcfa5d02108f54c57b0b7189bd05d0
Raphael Saldanha <rfsaldanha@gmail.com>
2023-05-29 10:29:12 +0200

    Update test.

--- tests/testthat/test-regsaude_female_pop.R
+++ tests/testthat/test-regsaude_female_pop.R
@@ -7,7 +7,7 @@ test_that("regsaude_female_pop works", {
 test_that("regsaude_female_pop with reg saude 449 works", {
   res <- regsaude_female_pop(type = "reg_saude_449")
 
-  expect_equal(178190, nrow(res))
+  expect_equal(178200, nrow(res))
 })
 
 test_that("regsaude_female_pop error", {
