f0bec218be65963ac150898e093e93c2122b90fc
Raphael Saldanha <rfsaldanha@gmail.com>
2023-05-29 09:32:13 +0200

    reg_saude_449 documentation

---
+++ R/mun_reg_saude_449.R
@@ -0,0 +1,10 @@
+#' Municipality and health region (449) code table
+#'
+#' A dataset containing municipality and health regions codes, following the 449 units specification.
+#'#'
+#' @format A tibble with 5,597 rows and 2 variables:
+#' \describe{
+#'   \item{cod_mun}{municipality 6 digits code}
+#'   \item{cod_reg_saude}{health region 5 digits code}
+#' }
+"mun_reg_saude_449"
