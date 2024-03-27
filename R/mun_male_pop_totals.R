df878f2fe3fd2acc4f04962c01551537ad470794
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-27 18:02:07 +0100

    Add source argument

--- R/mun_male_pop_totals.R
+++ R/mun_male_pop_totals.R
@@ -1,23 +1,24 @@
 #' Municipality yearly male population estimates totals
 #'
-#' This function provides a tibble containing male population estimates for Brazilian municipalities totals from 2000 to 2021.
+#' This function provides a tibble containing male population estimates for Brazilian municipalities totals.
 #'
-#' The estimates were calculated by DataSUS (Brazilian Ministry of Health), manually downloaded from DataSUS website, and organized as a tibble.
-#'
-#' \describe{
-#'   \item{mun}{municipality 6 digits code}
-#'   \item{year}{year of the estimative}
-#'   \item{pop}{population estimative}
-#' }
+#' @param source character. `bmh` for Brazilian Health Ministry estimates, or `ufrn` for UFRN-DEM-LEPP estimates.
 #'
 #' @returns A tibble.
-#' @seealso [mun_male_pop].
+#' @seealso [bmh_mun_male_pop], [ufrn_mun_male_pop].
 #'
 #' @importFrom rlang .data
 #' @export
 
-mun_male_pop_totals <- function(){
-res <- brpop::mun_male_pop %>%
+mun_male_pop_totals <- function(source = "bmh"){
+  # Estimates source
+  if(source == "bmh"){
+    mun_male_pop <- brpop::bmh_mun_male_pop
+  } else if(source == "ufrn"){
+    mun_male_pop <- brpop::ufrn_mun_male_pop
+  }
+
+  res <- mun_male_pop %>%
     dplyr::filter(.data$age_group == "Total") %>%
     dplyr::arrange(.data$mun, .data$year)
 
