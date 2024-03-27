97223526ddda15c1f7de4759d8d3715d5365b6da
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-27 15:13:06 +0100

    Create data raw with correct file name

--- data-raw/bmh_mun_pop.R
+++ data-raw/bmh_mun_pop.R
@@ -1,3 +1,13 @@
 ## code to prepare `bmh_mun_pop` dataset goes here
 
-usethis::use_data(bmh_mun_pop, overwrite = TRUE)
+load(file = "data-raw/bhm/mun_female_pop.rda")
+
+bmh_mun_female_pop <- mun_female_pop
+
+usethis::use_data(bmh_mun_female_pop, overwrite = TRUE, compress = "xz")
+
+load(file = "data-raw/bhm/mun_male_pop.rda")
+
+bmh_mun_male_pop <- mun_male_pop
+
+usethis::use_data(bmh_mun_male_pop, overwrite = TRUE, compress = "xz")
