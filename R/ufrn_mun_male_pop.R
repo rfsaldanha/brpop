9203d239986b86799af8b1acdbd37a40a626b8e8
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-27 15:19:28 +0100

    Document the UFRN datasets

---
+++ R/ufrn_mun_male_pop.R
@@ -0,0 +1,7 @@
+#' Municipality yearly male population estimates per age group from UFRN-DEM-LEPP
+#'
+#' A dataset containing male population estimates for Brazilian municipalities per age groups from 2010 to 2030.
+#'
+#' The estimates were computed by the LEPP laboratory from the Demography Department/UFRN.
+#'
+"ufrn_mun_male_pop"
