46176e2cf6748b82efeec421ae9024b93e282d8d
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-28 10:10:38 +0100

    Add source argument.

--- R/uf_sex_pop.R
+++ R/uf_sex_pop.R
@@ -1,18 +1,32 @@
 #' UF yearly population estimates per age group and sex
 #'
-#' This function binds UF male and female datasets (`uf_male_pop` and `uf_female_pop`) and includes a new variable called 'sex'.
+#' This function binds municipality male and female estimates and includes a new variable called 'sex'.
+#'
+#' @param source character. `bmh` for Brazilian Health Ministry estimates, or `ufrn` for UFRN-DEM-LEPP estimates.
 #'
 #' @returns A tibble.
-#' @seealso [uf_male_pop], [uf_female_pop].
+#' @seealso [bmh_mun_male_pop], [bmh_mun_female_pop], [ufrn_mun_male_pop], [ufrn_mun_female_pop].
 #'
 #' @importFrom rlang .data
 #' @export
 
-uf_sex_pop <- function(){
-  male <- brpop::uf_male_pop %>%
+uf_sex_pop <- function(source = "bmh"){
+  # Assertions
+  checkmate::assert_choice(x = source, choices = c("bmh", "ufrn"))
+
+  # Estimates source
+  if(source == "bmh"){
+    mun_male_pop <- brpop::bmh_mun_male_pop
+    mun_female_pop <- brpop::bmh_mun_female_pop
+  } else if(source == "ufrn"){
+    mun_male_pop <- brpop::ufrn_mun_male_pop
+    mun_female_pop <- brpop::ufrn_mun_female_pop
+  }
+
+  male <- mun_male_pop %>%
     dplyr::mutate(sex = "Male")
 
-  female <- brpop::uf_female_pop %>%
+  female <- mun_female_pop %>%
     dplyr::mutate(sex = "Female")
 
   res <- dplyr::bind_rows(male, female)
