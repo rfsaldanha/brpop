225b9a58b3ffdd43cdc217ce074653652086e29c
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-28 10:26:44 +0100

    Add source argument.

--- R/regsaude_pop.R
+++ R/regsaude_pop.R
@@ -1,18 +1,9 @@
 #' Health region yearly population estimates per age group
 #'
-#' This function provides a tibble containing population estimates for Brazilian health regions per age groups from 2000 to 2021.
+#' This function provides a tibble containing population estimates for Brazilian health regions per age groups.
 #'
 #' @param type character. 'standard' or 'reg_saude_449'
-#'
-#' The estimates were calculated by DataSUS (Brazilian Ministry of Health), manually downloaded from DataSUS website, and organized as a tibble.
-#'
-#' @format A tibble.
-#' \describe{
-#'   \item{regsaude}{health region 4 or 5 digits code}
-#'   \item{year}{year of the estimative}
-#'   \item{age_group}{age group}
-#'   \item{pop}{population estimative}
-#' }
+#' @param source character. `bmh` for Brazilian Health Ministry estimates, or `ufrn` for UFRN-DEM-LEPP estimates.
 #'
 #' @returns A tibble.
 #' @seealso [regsaude_male_pop], [regsaude_female_pop].
@@ -20,27 +11,26 @@
 #' @importFrom rlang .data
 #' @export
 
-regsaude_pop <- function(type = "standard"){
+regsaude_pop <- function(type = "standard", source = "bmh"){
+  # Assertions
+  checkmate::assert_choice(x = type, choices = c("standard", "reg_saude_449"))
+  checkmate::assert_choice(x = source, choices = c("bmh", "ufrn"))
 
-  if(!(type %in% c("standard", "reg_saude_449"))){
-    stop("type must be 'standard' or 'reg_saude_449'")
-  }
-
-  cluster <- multidplyr::new_cluster(n = future::availableCores(omit = 1))
 
   if(type == "standard"){
-    res <- dplyr::bind_rows(regsaude_male_pop(), regsaude_female_pop())
+    res <- dplyr::bind_rows(regsaude_male_pop(source = source),
+                            regsaude_female_pop(source = source))
   } else if(type == "reg_saude_449"){
-    res <- dplyr::bind_rows(regsaude_male_pop(type = "reg_saude_449"), regsaude_female_pop(type = "reg_saude_449"))
+    res <- dplyr::bind_rows(regsaude_male_pop(type = "reg_saude_449", source = source),
+                            regsaude_female_pop(type = "reg_saude_449", source = source))
   }
 
-  res <- res %>%
+  res <- dtplyr::lazy_dt(x = res) %>%
     dplyr::group_by(.data$regsaude, .data$year, .data$age_group) %>%
-    multidplyr::partition(cluster) %>%
     dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
     dplyr::collect() %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange(.data$regsaude, .data$year, .data$age_group)
+    dplyr::arrange(.data$regsaude, .data$year, .data$age_group) %>%
+    tibble::as_tibble()
 
   return(res)
 }
