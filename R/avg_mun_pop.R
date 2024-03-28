00abe40041aada67227cb812be3ad0a661b2f77c
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-28 15:24:35 +0100

    Refactor function.

--- R/avg_mun_pop.R
+++ R/avg_mun_pop.R
@@ -7,20 +7,19 @@
 #'
 avg_mun_pop <- function(sex){
   if(sex == "male"){
-    bmh_mun <- brpop::bmh_mun_male_pop %>%
-      dplyr::rename(pop_bmh = "pop")
-
-    ufrn_mun <- brpop::ufrn_mun_male_pop %>%
-      dplyr::rename(pop_ufrn = "pop") %>%
-      dplyr::mutate(mun = as.numeric(substr(.data$mun, 0, 6)))
+    bmh_mun <- brpop::bmh_mun_male_pop
+    ufrn_mun <- brpop::ufrn_mun_male_pop
   } else if(sex == "female"){
-    bmh_mun <- brpop::bmh_mun_female_pop %>%
-      dplyr::rename(pop_bmh = "pop")
-
-    ufrn_mun <- brpop::ufrn_mun_female_pop %>%
-      dplyr::rename(pop_ufrn = "pop") %>%
-      dplyr::mutate(mun = as.numeric(substr(.data$mun, 0, 6)))
+    bmh_mun <- brpop::bmh_mun_female_pop
+    ufrn_mun <- brpop::ufrn_mun_female_pop
   }
+
+  bmh_mun <- bmh_mun %>%
+    dplyr::rename(pop_bmh = "pop")
+
+  ufrn_mun <- ufrn_mun %>%
+    dplyr::mutate(mun = as.numeric(substr(.data$mun, 0, 6))) %>%
+    dplyr::rename(pop_ufrn = "pop")
 
   res <- dplyr::full_join(bmh_mun, ufrn_mun,
                           by = c("mun", "year", "age_group")) %>%
