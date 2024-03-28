81e91c3011d692d1e50754b39419b25a1e05bef6
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-28 10:05:43 +0100

    Add source argument.

--- R/uf_female_pop_totals.R
+++ R/uf_female_pop_totals.R
@@ -1,25 +1,35 @@
 #' UF yearly female population estimates totals
 #'
-#' This function provides a tibble containing female population estimates for Brazilian UFs ("Unidades Federativas") totals from 2000 to 2021.
+#' This function provides a tibble containing female population estimates for Brazilian UFs ("Unidades Federativas") totals.
 #'
-#' The estimates were calculated by DataSUS (Brazilian Ministry of Health), manually downloaded from DataSUS website, and organized as a tibble.
-#'
-#' \describe{
-#'   \item{uf}{UF 2 digits code}
-#'   \item{year}{year of the estimative}
-#'   \item{pop}{population estimative}
-#' }
+#' @param source character. `bmh` for Brazilian Health Ministry estimates, or `ufrn` for UFRN-DEM-LEPP estimates.
 #'
 #' @returns A tibble.
-#' @seealso [uf_female_pop].
+#' @seealso [bmh_mun_female_pop], [ufrn_mun_female_pop].
 #'
 #' @importFrom rlang .data
 #' @export
 
-uf_female_pop_totals <- function(){
-res <- brpop::uf_female_pop %>%
+uf_female_pop_totals <- function(source = "bmh"){
+  # Assertions
+  checkmate::assert_choice(x = source, choices = c("bmh", "ufrn"))
+
+  # Estimates source
+  if(source == "bmh"){
+    mun_female_pop <- brpop::bmh_mun_female_pop
+  } else if(source == "ufrn"){
+    mun_female_pop <- brpop::ufrn_mun_female_pop
+  }
+
+  res <- mun_female_pop %>%
     dplyr::filter(.data$age_group == "Total") %>%
-    dplyr::arrange(.data$uf, .data$year)
+    dplyr::mutate(uf = substr(.data$mun, 0, 2)) %>%
+    dtplyr::lazy_dt() %>%
+    dplyr::group_by(.data$uf, .data$year) %>%
+    dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(.data$uf, .data$year, .data$pop) %>%
+    tibble::as_tibble()
 
   return(res)
 }
