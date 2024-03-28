6df4eca0d2d66ae66c257a7be7e7c22fa177887a
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-28 11:58:31 +0100

    Tests update

--- tests/testthat/test-regsaude_female_pop.R
+++ tests/testthat/test-regsaude_female_pop.R
@@ -1,15 +1,25 @@
-test_that("regsaude_female_pop works", {
-  res <- regsaude_female_pop()
+test_that("bmh regsaude_female_pop works", {
+  res <- regsaude_female_pop(source = "bmh")
 
   expect_equal(143748, nrow(res))
 })
 
-test_that("regsaude_female_pop with reg saude 449 works", {
-  res <- regsaude_female_pop(type = "reg_saude_449")
+test_that("ufrn regsaude_female_pop works", {
+  res <- regsaude_female_pop(source = "ufrn")
+
+  expect_equal(141540, nrow(res))
+})
+
+test_that("bmh regsaude_female_pop with reg saude 449 works", {
+  res <- regsaude_female_pop(type = "reg_saude_449", source = "bmh")
 
   expect_equal(178200, nrow(res))
 })
 
-test_that("regsaude_female_pop error", {
-  expect_error(regsaude_female_pop(type = "blabla"))
+test_that("ufrn regsaude_female_pop with reg saude 449 works", {
+  res <- regsaude_female_pop(type = "reg_saude_449", source = "ufrn")
+
+  expect_equal(188580, nrow(res))
 })
+
+
--- tests/testthat/test-regsaude_male_pop.R
+++ tests/testthat/test-regsaude_male_pop.R
@@ -1,15 +1,25 @@
-test_that("regsaude_male_pop works", {
-  res <- regsaude_male_pop()
+test_that("bmh regsaude_male_pop works", {
+  res <- regsaude_male_pop(source = "bmh")
 
   expect_equal(143748, nrow(res))
 })
 
-test_that("regsaude_male_pop with reg saude 449 works", {
-  res <- regsaude_male_pop(type = "reg_saude_449")
+test_that("ufrn regsaude_male_pop works", {
+  res <- regsaude_male_pop(source = "ufrn")
+
+  expect_equal(141540, nrow(res))
+})
+
+test_that("bmh regsaude_male_pop with reg saude 449 works", {
+  res <- regsaude_male_pop(type = "reg_saude_449", source = "bmh")
 
   expect_equal(178200, nrow(res))
 })
 
-test_that("regsaude_male_pop error", {
-  expect_error(regsaude_male_pop(type = "blabla"))
+test_that("ufrn regsaude_male_pop with reg saude 449 works", {
+  res <- regsaude_male_pop(type = "reg_saude_449", source = "ufrn")
+
+  expect_equal(188580, nrow(res))
 })
+
+
