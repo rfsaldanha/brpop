b1442ba6fdbaa6df705f4714508252dfc6cb5250
Raphael Saldanha <rfsaldanha@gmail.com>
2024-03-28 09:01:26 +0100

    Add source argument.

--- R/uf_pop.R
+++ R/uf_pop.R
@@ -1,34 +1,36 @@
 #' UF yearly population estimates per age group
 #'
-#' This function provides a tibble containing population estimates for Brazilian UFs ("Unidades Federativas") per age groups from 2000 to 2021.
+#' This function provides a tibble containing population estimates for Brazilian UFs ("Unidades Federativas") per age groups.
 #'
-#' The estimates were calculated by DataSUS (Brazilian Ministry of Health), manually downloaded from DataSUS website, and organized as a tibble.
-#'
-#' @format A tibble with 10,692 rows and 4 variables:
-#' \describe{
-#'   \item{uf}{UF 2 digits code}
-#'   \item{year}{year of the estimative}
-#'   \item{age_group}{age group}
-#'   \item{pop}{population estimative}
-#' }
+#' @param source character. `bmh` for Brazilian Health Ministry estimates, or `ufrn` for UFRN-DEM-LEPP estimates.
 #'
 #' @returns A tibble.
-#' @seealso [uf_male_pop], [uf_female_pop].
+#' @seealso [bmh_mun_male_pop], [bmh_mun_female_pop], [ufrn_mun_male_pop], [ufrn_mun_female_pop].
 #'
 #' @importFrom rlang .data
 #' @export
 
-uf_pop <- function(){
+uf_pop <- function(source = "bmh"){
+  # Assertions
+  checkmate::assert_choice(x = source, choices = c("bmh", "ufrn"))
 
-  cluster <- multidplyr::new_cluster(n = future::availableCores(omit = 1))
+  # Estimates source
+  if(source == "bmh"){
+    mun_male_pop <- brpop::bmh_mun_male_pop
+    mun_female_pop <- brpop::bmh_mun_female_pop
+  } else if(source == "ufrn"){
+    mun_male_pop <- brpop::ufrn_mun_male_pop
+    mun_female_pop <- brpop::ufrn_mun_female_pop
+  }
 
-  res <- dplyr::bind_rows(brpop::uf_male_pop, brpop::uf_female_pop) %>%
+  res <- dplyr::bind_rows(mun_male_pop, mun_female_pop) %>%
+    dplyr::mutate(uf = substr(.data$mun, 0, 2)) %>%
+    dtplyr::lazy_dt() %>%
     dplyr::group_by(.data$uf, .data$year, .data$age_group) %>%
-    multidplyr::partition(cluster) %>%
     dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
-    dplyr::collect() %>%
     dplyr::ungroup() %>%
-    dplyr::arrange(.data$uf, .data$year, .data$age_group, .data$pop)
+    dplyr::arrange(.data$uf, .data$year, .data$age_group, .data$pop) %>%
+    tibble::as_tibble()
 
   return(res)
 }
