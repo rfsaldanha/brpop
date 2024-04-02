5139b6e632115aaec8d19e9fea1204634b51d124
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-02 15:04:40 +0200

    Add ibge source

--- R/uf_pop_totals.R
+++ R/uf_pop_totals.R
@@ -1,18 +1,18 @@
-#' UF yearly population estimates totals
+#' UF yearly population totals
 #'
-#' This function provides a tibble containing population estimates for Brazilian UFs ("Unidades Federativas") totals.
+#' This function provides a tibble containing total population estimates for Brazilian UFs ("Unidades Federativas") totals.
 #'
-#' @param source character. `bmh` for Brazilian Health Ministry estimates, or `ufrn` for UFRN-DEM-LEPP estimates.
+#' @param source character. `bmh` for Brazilian Health Ministry estimates (2000 to 2021), `ufrn` for UFRN-DEM-LEPP estimates (2010 to 2030), or `ibge` for IBGE estimates (2000 to 2022).
 #'
 #' @returns A tibble.
-#' @seealso [bmh_mun_male_pop], [bmh_mun_female_pop], [ufrn_mun_male_pop], [ufrn_mun_female_pop].
+#' @seealso [bmh_mun_male_pop], [bmh_mun_female_pop], [ufrn_mun_male_pop], [ufrn_mun_female_pop], [ibge_pop].
 #'
 #' @importFrom rlang .data
 #' @export
 
 uf_pop_totals <- function(source = "bmh"){
   # Assertions
-  checkmate::assert_choice(x = source, choices = c("bmh", "ufrn"))
+  checkmate::assert_choice(x = source, choices = c("bmh", "ufrn", "ibge"))
 
   # Estimates source
   if(source == "bmh"){
@@ -23,15 +23,27 @@ uf_pop_totals <- function(source = "bmh"){
     mun_female_pop <- brpop::ufrn_mun_female_pop
   }
 
-  res <- dplyr::bind_rows(mun_male_pop, mun_female_pop) %>%
-    dplyr::filter(.data$age_group == "Total") %>%
-    dplyr::mutate(uf = substr(.data$mun, 0, 2)) %>%
-    dtplyr::lazy_dt() %>%
-    dplyr::group_by(.data$uf, .data$year) %>%
-    dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange(.data$uf, .data$year, .data$pop) %>%
-    tibble::as_tibble()
+  if(source == "bmh" | source == "ufrn"){
+    res <- dplyr::bind_rows(mun_male_pop, mun_female_pop) %>%
+      dplyr::filter(.data$age_group == "Total") %>%
+      dplyr::mutate(uf = substr(.data$mun, 0, 2)) %>%
+      dtplyr::lazy_dt() %>%
+      dplyr::group_by(.data$uf, .data$year) %>%
+      dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
+      dplyr::ungroup() %>%
+      dplyr::arrange(.data$uf, .data$year, .data$pop) %>%
+      tibble::as_tibble()
+  } else if (source == "ibge"){
+    res <- ibge_pop %>%
+      dplyr::mutate(code_uf = substr(.data$code_muni, 0, 2)) %>%
+      dtplyr::lazy_dt() %>%
+      dplyr::group_by(.data$code_uf, .data$year) %>%
+      dplyr::summarise(pop = sum(.data$pop, na.rm = TRUE)) %>%
+      dplyr::ungroup() %>%
+      dplyr::arrange(.data$code_uf, .data$year, .data$pop) %>%
+      tibble::as_tibble()
+  }
+
 
   return(res)
 }
